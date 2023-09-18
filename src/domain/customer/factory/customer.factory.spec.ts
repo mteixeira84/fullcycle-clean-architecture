df80d16e48dc90c48c219ae2b9ee52fdb5f1bf7d
Matheus Teixeira <matheus.analista@gmail.com>
2023-09-18 15:14:22 -0300

    feat: implements factory

---
+++ src/domain/customer/factory/customer.factory.spec.ts
@@ -0,0 +1,21 @@
+import Address from "../value-object/address";
+import CustomerFactory from "./customer.factory";
+
+describe("Customer factory unit test", () => {
+  it("should create a customer", () => {
+    let customer = CustomerFactory.create("John");
+
+    expect(customer.id).toBeDefined();
+    expect(customer.name).toBe("John");
+    expect(customer.Address).toBeUndefined();
+  });
+
+  it("should create a customer with an address", () => {
+    const address = new Address("Street", 1, "12345-678", "City");
+    let customer = CustomerFactory.createWithAddress("John", address);
+
+    expect(customer.id).toBeDefined();
+    expect(customer.name).toBe("John");
+    expect(customer.Address).toBe(address);
+  });
+});
