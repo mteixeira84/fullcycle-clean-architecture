400ea960ffb9f3aff9cc52688404aa937da70fbc
Matheus Teixeira <matheus.analista@gmail.com>
2023-10-23 14:18:00 -0300

    feat: implements presenters with output xml

--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
 import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
 import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usecase";
+import CustomerPresenter from "../presenters/customer.presenter";
 
 export const customerRoute = express.Router();
 
@@ -26,11 +27,10 @@ customerRoute.post("/", async (req: Request, res: Response) => {
 
 customerRoute.get("/", async (req: Request, res: Response) => {
   const usecase = new ListCustomerUseCase(new CustomerRepository());
-  console.log(usecase);
-  try {
-    const output = await usecase.execute({});
-    res.send(output);
-  } catch (err) {
-    res.status(500).send(err);
-  }
+  const output = await usecase.execute({});
+
+  res.format({
+    json: async () => res.send(output),
+    xml: async () => res.send(CustomerPresenter.listXML(output)),
+  });
 });
